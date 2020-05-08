91bf12abd89744198f6478c678960c934509aec8
soumit96 <54953909+soumit96@users.noreply.github.com>
2020-05-08 23:10:50 +0530

    Add files via upload
    Version_03_almost_Final

--- new_GardenDB.sql
+++ new_GardenDB.sql
@@ -43,7 +43,7 @@ CREATE TABLE Weather (
 	precipitation		INT				NOT NULL,
 	overcast			INT				NOT NULL,
 	windSpeed			INT				NOT NULL,
-	windDirection		CHAR	 		NOT NULL	DEFAULT('N'),
+	windDirection		VARCHAR(5)	 	NOT NULL	DEFAULT('N'),
 	recordedDate		DATETIME		NOT NULL
 );
 
@@ -106,13 +106,169 @@ CREATE TABLE Tended (
 	recordedDate		DATETIME		NOT NULL,
 	plantCondition		VARCHAR(30)		NOT NULL
 );
+GO
 
 
+/************************************************************************************************************  
+												CREATE VIEWS
+*************************************************************************************************************/
+CREATE VIEW vwPlantList AS															-- No. 1
+	SELECT p.plantId, pt.plantName, pt.plantBreed, pt.[description]
+	FROM Plant p, PlantType pt 
+	WHERE p.plantTypeId = pt.plantTypeId AND p.archived = 0
+GO
 
--- ALTER TABLE Harvest ADD FOREIGN KEY (plantId) REFERENCES Plant(plantId);
---ALTER TABLE Tended	ADD FOREIGN KEY (plantId) REFERENCES Plant(plantId);
+
+CREATE VIEW vwTypeList AS															-- No. 2
+	SELECT pt.plantName, pt.plantBreed, pt.description
+	FROM plantType pt
+GO
+
+
+CREATE VIEW vwWateredList AS														-- No. 3
+	SELECT DISTINCT p.plantId, pt.plantName, pt.plantBreed, t.recordedDate, a.watered
+	FROM	(Plant p	LEFT JOIN Tended t ON p.plantId = t.plantId 
+						LEFT JOIN ActionTbl a ON t.actionId = a.actionId 
+						LEFT JOIN PlantType pt ON pt.plantTypeId = p.plantTypeId
+			)
+	WHERE a.watered = 1
+GO
+
+
+CREATE VIEW vwDepestedList AS														-- No. 4
+	SELECT DISTINCT p.plantId, pt.plantName, pt.plantBreed, t.recordedDate, a.watered
+	FROM	(Plant p	LEFT JOIN Tended t ON p.plantId = t.plantId 
+						LEFT JOIN ActionTbl a ON t.actionId = a.actionId 
+						LEFT JOIN PlantType pt ON pt.plantTypeId = p.plantTypeId
+			)
+	WHERE a.depested = 1
+GO
+
+CREATE VIEW vwFertilizedList AS														-- No. 5
+	SELECT DISTINCT p.plantId, pt.plantName, pt.plantBreed, t.recordedDate, a.watered
+	FROM	(Plant p	LEFT JOIN Tended t ON p.plantId = t.plantId 
+						LEFT JOIN ActionTbl a ON t.actionId = a.actionId 
+						LEFT JOIN PlantType pt ON pt.plantTypeId = p.plantTypeId
+			)
+	WHERE a.fertilized = 1
+GO
+
+--CREATE VIEW vwPlantedList AS
+--	SELECT	p.plantId, pt.plantName, pt.plantBreed, t.recordedDate, a.planted
+--	FROM	(plantTbl p	LEFT JOIN tendedTbl t ON p.plantId = t.plantId 
+--						LEFT JOIN actionTbl a ON t.actionId = a.actionId 
+--						LEFT JOIN plantTypeTbl pt ON pt.plantTypeId = p.plantTypeId
+--			)
+--	WHERE p.plantId = t.plantId AND a.planted = 1
+--GO
+
+CREATE VIEW vwHarvestDate AS			-- projected harvest date
+	SELECT p.plantId, pt.daysToHarvest, t.recordedDate AS [datePlanted], DATEADD(day, pt.daysToHarvest, t.recordedDate) AS [harvestDate]
+	FROM Plant p LEFT JOIN PlantType pt ON p.plantTypeId = pt.plantTypeId, Tended t 
+					LEFT JOIN ActionTbl a on t.actionId = a.actionId
+
+	WHERE p.plantId = t.plantId
+GO
+
+CREATE VIEW vwPlantLocation AS														-- No. 6
+	SELECT p.plantId, l.fieldName, l.fieldColumn, l.fieldRow 
+	FROM Plant p JOIN LocationTbl l on p.locationId = l.locationId
 
 GO
+
+/********************************************************************************************************************************************
+																INSERT DATA
+*********************************************************************************************************************************************/
+
+SET IDENTITY_INSERT Weather ON
+
+INSERT INTO Weather (weatherId, humidity, temperature, precipitation, overcast, windSpeed, windDirection, recordedDate) VALUES 
+(1, 37, 39, 63, 35, 25, 'SE', '5/8/2020'),
+(2, 68, 81, 46, 79, 62, 'N', '5/7/2020'),
+(3, 9, 34, 62, 68, 57, 'NE', '5/6/2020'),
+(4, 85, 123, 95, 70, 35, 'E', '5/3/2020'),
+(5, 64, 67, 12, 65, 63, 'W', '4/30/2020'),
+(6, 60, 68, 64, 5, 8, 'N', '4/21/2020'),
+(7, 88, 104, 98, 26, 37, 'W', '4/20/2020'),
+(8, 82, 100, 89, 3, 63, 'W', '4/17/2020'),
+(9, 67, 12, 50, 21, 60, 'NW', '4/12/2020'),
+(10, 20, 37, 50, 78, 4, 'NE', '4/10/2020'),
+(11, 24, 90, 49, 72, 53, 'NW', '9/23/2019'),
+(12, 57, 121, 85, 95, 23, 'N', '4/18/2020')
+
+SET IDENTITY_INSERT Weather OFF
+
+Select * from Weather
+
+
+INSERT INTO plantType (plantName, plantBreed, daysToHarvest, [description] ) VALUES 
+('Cilantro', 'Common', 45 , 'The highly aromatic, rich and spicy Cilantro adds the perfect flavor to any cuisine! This plant is ideal for harvesting both the cilantro leaves and coriander seeds. The unique flavor of this cilantro is bold and bright with a touch of citrus undertones.'),
+('Cilantro', 'Coriandrum sativum', 80 , 'Coriander is an annual herb in the family Apiaceae. It is also known as Chinese parsley, and in the United States the stems and leaves are usually called cilantro'),
+('Corn', 'Bilicious Hybrid', 78, 'Bi-Licious is an excellent mid-season hybrid with bi-colored kernels maturing in 75 or more days. Ears are 8 1/2 inches with 16 rows of kernels. Corn is a warm season crop. Pollination is essential for success so corn needs to be planted in a block of three rows of 12-24 plants (rather than one row).'),
+('Kohlrabi', 'Early White Vienna', 55, 'Round, above-ground "bulbs" with light green, smooth skin have creamy white, tender flesh. Flavor is mild, sweet, turnip-like. Superb raw or steamed. Ready for harvest 55 days from seed sowing.'),
+('Hot Pepper', 'Jalapeno Early', 80, 'Cooks Garden Favorite. Dark green, pungent 3" hot peppers are excellent fresh or pickled. Zesty flavor is great in Mexican dishes.'),
+('Watermelon', 'Orange Tendersweet', 90, 'This heirloom favorite features luscious, bright orange flesh. Tender and very sweet, the oblong striped fruits grow to 35 lb.'),
+('Cucumber', 'PickelBush', 70, 'Burpee-bred Picklebush has unbelievably compact vines that get only 2 long. White-spined fruits have classic pickle look, deep green with paler stripes. Up to 4 1/2" long, 1 1/2" across at maturity, but use them at any size. Very productive and tolerant to powdery mildew and cucumber mosaic virus.'),
+('Tomato', 'Roma ', 80, 'Compact plants produce paste-type tomatoes resistant to Verticillium and Fusarium wilts. Meaty interiors and few seeds. GARDEN HINTS: Fertilize when first fruits form to increase yield. Water deeply once a week during very dry weather. We searched the world to find the best organic seed-Burpee fully guarantees that not a drop of synthetic chemicals was used to make these excellent seeds.')
+
+select * from PlantType
+
+
+
+SET IDENTITY_INSERT locationTbl ON
+INSERT INTO locationTbl  (locationId, fieldName, fieldColumn, fieldRow) VALUES 
+(1, 'NORTH',   1, 1),
+(2, 'NORTH',   1, 2),
+(3, 'NORTH',   1, 3),
+(4, 'NORTH',   1, 4),
+(5, 'NORTH',   1, 5),
+(6, 'NORTH',   1, 6),
+(7, 'NORTH',   1, 7),
+(8, 'NORTH',   1, 8),
+(9, 'NORTH',   1, 9),
+(10, 'NORTH',  1, 10),
+(11, 'NORTH',  1, 11),
+(12, 'NORTH',  1, 12)
+SET IDENTITY_INSERT locationTbl OFF
+
+select * from LocationTbl
+
+
+
+
+
+SET IDENTITY_INSERT Plant ON
+INSERT INTO Plant (plantId, plantTypeId, locationId) VALUES 
+(1,   1,  1),
+(2,   2,  2),
+(3,   5,  3),
+(4,   4,  4),
+(5,   4,  5),
+(6,   5,  6),
+(7,   2,  7),
+(8,   3,  8),
+(9,   6,  9),
+(10,  1, 10),
+(11,  2, 11)
+SET IDENTITY_INSERT Plant OFF
+
+
+
+
+SET IDENTITY_INSERT Harvest ON
+INSERT INTO Harvest (harvestId, plantId, recordedDate, numHarvest, numWaste) 
+VALUES 
+(1,	1, '6/10/2020', 8, 18),
+(2,	2, '11/15/2020', 51, 3),
+(3, 3, '12/10/2020', 48, 8),
+(4, 4, '11/13/2020', 86, 16),
+(5, 5, '10/28/2020', 88, 6),
+(6, 6, '2/5/2020', 62, 14)
+SET IDENTITY_INSERT Harvest OFF
+
+
+
+
 
 
 
@@ -236,7 +392,7 @@ GO
 --SELECT * FROM Tended 
 
 --GO
-
+GO
 
 /*=====================================================================================================================================
 														 STORED PROCEEDURES
@@ -251,7 +407,7 @@ CREATE PROCEDURE spAddUpdateDelete_Weather
 	@precipitation		INT,	
 	@overcast			INT,	
 	@windSpeed			INT,	
-	@windDirection		CHAR,	 
+	@windDirection		VARCHAR(5),	 
 	@recordedDate		DATETIME,
 	@delete				BIT = 0
 AS BEGIN
@@ -370,9 +526,9 @@ END CATCH
 END
 GO
 
-exec spAddUpdateDelete_PlantType 0, 'Tomato', 'Roma', 27, 'juicy tomato', 0
+--exec spAddUpdateDelete_PlantType 0, 'Tomato', 'Roma', 27, 'juicy tomato', 0
 
-GO
+
 
                                     --*************** ADD/UPDATE Harvest *****************
 CREATE PROCEDURE spAddUpdate_Harvest
@@ -446,7 +602,7 @@ AS BEGIN
 		END ELSE BEGIN
 			
 			UPDATE ActionTbl
-			SET planted = @planted, watered = @planted, fertilized = @fertilized, depested = @depested, noAction = @noAction
+			SET planted = @planted, watered = @watered, fertilized = @fertilized, depested = @depested, noAction = @noAction
 			WHERE actionId = @actionId
 			SELECT	@actionId AS actionId,
 					[success] = CAST(1 AS BIT)
@@ -601,7 +757,7 @@ BEGIN TRY
 		END
 
 	END ELSE BEGIN						-- UPDATE plant
-		IF NOT EXISTS (SELECT TOP(1) NULL FROM Plant WHERE PlantId = @plantTypeId) BEGIN
+		IF NOT EXISTS (SELECT TOP(1) NULL FROM Plant WHERE PlantId = @plantId) BEGIN
 			SELECT	[message] = 'plant ID does not exist',
 					[success] = CAST(0 AS BIT)
 		END ELSE BEGIN
@@ -673,9 +829,182 @@ END
 GO
 
 
+/************************************************************************************************************************************************
+
+															GETTERS PROCEDURES
+
+*************************************************************************************************************************************************/
 
 
+/* =====================================================================
 
+	Name:           spGetPlantBreedByName
+	Purpose:        Gets a list of all plants from the database by name.
+
+======================================================================== */
+GO
+CREATE PROCEDURE spGetPlantBreedByName
+	@plantBreedName		VARCHAR(64),
+	@hardMatch			BIT = 0
+AS BEGIN
+	IF (@hardMatch = 1) BEGIN
+		SELECT *
+		FROM vwTypeList tl
+		WHERE tl.plantBreed = @plantBreedName
+	END ELSE BEGIN
+		SELECT *
+		FROM vwTypeList tl
+		WHERE tl.plantBreed LIKE CONCAT('%', @plantBreedName, '%')
+	END
+END
+GO
+
+/* =====================================================================
+
+	Name:           spGetPlantByTypeName
+	Purpose:        Gets a list of all plants from the database by name.
+
+======================================================================== */
+GO
+CREATE PROCEDURE spGetPlantByTypeName
+	@plantType				VARCHAR(64),
+	@hardMatch				BIT = 0
+AS BEGIN
+	IF (@hardMatch = 1) BEGIN
+		SELECT *
+		FROM vwTypeList tl
+		WHERE tl.plantName = @plantType
+	END ELSE BEGIN
+		SELECT *
+		FROM vwTypeList tl
+		WHERE tl.plantName LIKE CONCAT('%', @plantType, '%')
+	END
+END
+GO
+
+/* =====================================================================
+
+	Name:           spGetWeatherByDate
+	Purpose:        Get all weather data between a date range
+
+======================================================================== */
+GO
+CREATE PROCEDURE spGetWeatherByDate
+	@startDate				DATETIME,
+	@endDate				DATETIME
+AS BEGIN
+		SELECT *
+		FROM weatherTbl	w
+		WHERE w.recordedDate BETWEEN @startDate AND @endDate
+END
+GO
+
+
+SELECT * FROM weatherTbl
+/* =====================================================================
+
+	Name:           spGetPlantConditionByDate
+	Purpose:        Get all plant conditions between a date range
+
+======================================================================== */
+GO
+CREATE PROCEDURE spGetPlantConditionByDate
+	@startDate				DATETIME,
+	@endDate				DATETIME
+AS BEGIN
+		SELECT t.plantId, t.recordedDate, t.plantCondition
+		FROM tendedTbl t
+		WHERE t.recordedDate BETWEEN @startDate AND @endDate
+END
+GO
+
+
+/* =====================================================================
+
+	Name:           spGetLocByPlantType
+	Purpose:        Get all weather data between a date range
+
+======================================================================== */
+GO
+CREATE PROCEDURE spGetLocByPlantType
+		@plantType		VARCHAR(64)
+AS BEGIN
+		SELECT DISTINCT pl.*, pt.plantName
+		FROM vwPlantLocation pl LEFT JOIN plantTbl p ON pl.plantId = p.plantTypeId, plantTypeTbl pt
+		WHERE pt.plantName LIKE CONCAT('%', 'Tomato', '%')
+END
+GO
+
+
+/* =====================================================================
+
+	Name:           spGetLocByPlantType
+	Purpose:        Get all weather data between a date range
+
+======================================================================== */
+GO
+CREATE PROCEDURE spGetSoilListByPh(
+		@startRange		FLOAT,
+		@endRange		FLOAT
+		)
+AS BEGIN
+
+		SELECT sc.*
+		FROM soilConditionTbl sc
+		WHERE sc.pH BETWEEN @startRange AND @endRange
+END
+GO
+/* =====================================================================
+
+	Name:           spGetHarvestsByDateRange
+	Purpose:        Get all weather data between a date range
+
+======================================================================== */
+GO
+CREATE PROCEDURE spGetHarvestsByDateRange
+	@startDate				DATETIME,
+	@endDate				DATETIME
+AS BEGIN
+
+		SELECT hd.plantId, hd.harvestDate
+		FROM vwHarvestDate hd
+		WHERE hd.harvestDate BETWEEN @startDate AND @endDate
+		GROUP BY hd.plantId, hd.harvestDate
+END
+GO
+
+
+/************************************************************* TEST PROCEDURES *********************************************************/
+
+-- Add weather
+	exec spAddUpdateDelete_Weather 0, 56, 76, 2, 25, 20, 'NW', '05/08/2020'
+	select * from Weather
+
+-- Update weather
+	exec spAddUpdateDelete_Weather 1, 26, 36, 3, 45, 50, 'W', '05/09/2020'
+	select * from Weather
+
+-- DELETE weather
+	exec spAddUpdateDelete_Weather 1, 26, 36, 3, 45, 50, 'W', '05/09/2020', 1
+	select * from Weather
+
+-- ADD plantType
+	exec spAddUpdateDelete_PlantType 0, 'Tomato', 'Roma', 20, 'juicy roma tomatoes ~ very delicious', 0
+	exec spAddUpdateDelete_PlantType 0, 'Tomato', 'Cherry', 25, 'cherrytomatoes ~ very delicious', 0
+	exec spAddUpdateDelete_PlantType 0, 'Tomato', 'Grape', 22, 'tart in taste', 0
+	select * from PlantType
+
+-- Update plantType
+	exec spAddUpdateDelete_PlantType 1, 'Tomato', 'Roma', 18, 'juicy roma tomatoes ~ very delicious [UPDATED]', 0
+	select * from PlantType
+
+-- SOFT DELETE plantType
+	exec spAddUpdateDelete_PlantType 2, 'Tomato', 'Cherry', 25, 'cherrytomatoes ~ very delicious', 1
+	select * from PlantType
+
+
+-- Add location
+	exec spAddUpdate_Location 
 
 
 
